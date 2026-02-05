1e23de9dbd9a806faf2595984a4c1f73ab38315a
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-05 15:21:15 +0100

    validation: add requests for cuisine types

--- BookMyTableV1/app/Http/Requests/StoreTypeCuisineRequest.php
+++ BookMyTableV1/app/Http/Requests/StoreTypeCuisineRequest.php
@@ -14,7 +14,7 @@ class StoreTypeCuisineRequest extends FormRequest
         return false;
     }
 
-    /**
+    /** 
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
--- BookMyTableV1/app/Http/Requests/UpdateTypeCuisineRequest.php
+++ BookMyTableV1/app/Http/Requests/UpdateTypeCuisineRequest.php
@@ -22,7 +22,7 @@ class UpdateTypeCuisineRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            // 
         ];
     }
 }
